ee8255339b4766fb4b16a1a6ef80cf05714862ff
fairysperta <1678020420@qq.com>
2020-02-05 17:02:34 +0800

    Site updated: 2020-02-05 17:02:32

--- js/clipboard_use.js
+++ js/clipboard_use.js
@@ -1,3 +1,3 @@
-// build time:Wed Feb 05 2020 16:31:40 GMT+0800 (GMT+08:00)
+// build time:Wed Feb 05 2020 17:01:50 GMT+0800 (GMT+08:00)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
